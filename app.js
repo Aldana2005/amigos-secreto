f33169b4d8853afd6bd2cb02b39938baa08a4f5b
mary aldana <mary.aldana022@gmail.com>
2025-01-29 18:11:25 -0500

    Actualizacion del archivo app.js

--- app.js
+++ app.js
@@ -1,1 +1,48 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos =[];
+
+function agregarAmigo() {
+    let nombreAmigo = document.getElementById('amigo').value;
+    if (nombreAmigo === "") {
+        alert('Por favor, inserte un nombre');
+        return;
+    }else{
+        amigos.push(nombreAmigo);
+        limpiarCaja();
+        mostrarLista();
+    }
+    
+}
+
+function limpiarCaja(){
+    document.getElementById('amigo').value = '';
+}
+
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert('No hay amigos en la lista');
+        return;
+    }else{
+        let aleatorio = Math.floor(Math.random()* amigos.length);
+        let amigoSeleccionado = amigos[aleatorio];
+
+        document.getElementById('resultado').innerHTML = `El amigo sorteado es: ${amigoSeleccionado}`;
+        
+    }
+}
+
+function mostrarLista() {
+    let listaDeAmigos = document.getElementById('listaAmigos');
+    listaDeAmigos.innerHTML ='';
+
+    for (let i = 0; i < amigos.length; i++) {
+        let li = document.createElement('li');
+
+        li.textContent = amigos[i];
+        listaDeAmigos.appendChild(li);
+        
+    }
+    
+}
+
+
